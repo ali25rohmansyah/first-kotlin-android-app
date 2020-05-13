5ca562ffe781b79a94fce36ad08a3a65c27d6347
@ali25rohmansyah <ali25rohmansyah@gmail.com>
2020-05-13 23:45:57 +0700

    Add Challenge 6: Add a Property

--- app/src/main/java/com/raproject/first_kotlin_android_app/MainActivity.kt
+++ app/src/main/java/com/raproject/first_kotlin_android_app/MainActivity.kt
@@ -2,11 +2,21 @@ package com.raproject.first_kotlin_android_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnTap: Button
+    private lateinit var txtScore: TextView
+    private lateinit var txtTime: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnTap = findViewById(R.id.btn_tap)
+        txtScore = findViewById(R.id.txt_score)
+        txtTime = findViewById(R.id.txt_time)
     }
 }
